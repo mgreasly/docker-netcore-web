6a202aa90b267296bfb9d93ef733669c8b47f8ab
Marcus Greasly <marcus.greasly@avanade.com>
2021-06-25 14:47:24 +1000

    Added webapi with simple Hello World endpoint

---
+++ Controllers/HelloWorlds.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace docker_netcore_web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var rng = new Random();
+            var max = rng.Next(15) + 1;
+            for (var i = 0; i < max; i++)
+            {
+                yield return $"Hello world {i}";
+            }
+        }
+    }
+}
